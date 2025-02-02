2ca7d89d1f9414b6f2b47aa9d9577ec764c1ceab
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2025-02-02 12:21:46 +0530

    dfgfg

--- script.js
+++ script.js
@@ -114,3 +114,15 @@ keys.addEventListener('click', (event) => {
 
     inputDigit(target.value);
 });
+
+// Dark mode toggle
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
